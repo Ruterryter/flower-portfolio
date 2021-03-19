16b2db16fbe5c30a51c987a9fcc87bc1f98c92e7
Hanna Ruter <hanna.ruter@gmail.com>
2021-03-19 16:04:42 +0100

    new favicon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div>
       <p>Violets are blue</p>
+      <a href='https://www.vecteezy.com/free-vector/puerto-rico'>
+        Icon by Puerto Rico Vectors by Vecteezy
+      </a>
     </div>
   );
 }
